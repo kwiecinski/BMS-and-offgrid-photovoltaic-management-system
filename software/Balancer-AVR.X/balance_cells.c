e86283817e90ca91a5a503dde7afe23a849fe663
kwiecinski <senio5555@gmail.com>
2025-05-09 15:52:19 +0200

    Add soft-start for balancer

--- software/Balancer-AVR.X/balance_cells.c
+++ software/Balancer-AVR.X/balance_cells.c
@@ -17,12 +17,17 @@
 
 
 #define MAX_CELL_VOLTAGE_DIFFRENCE  10
+#define DEAD_VOLTAGE_DIFFRENCE      2
 #define MAX_CMP_VALUE               4000    
 #define PWM_24V_CMP_REGISTER        TCD0.CMPASET    // off=MAX_CMP_VALUE
 #define PWM_12V_CMP_REGISTER        TCD0.CMPBSET    // off=0
-#define KP                          35
+#define KP_12V                          35
+#define KP_24V                          35
 #define MINIMUM_CELL_VOLTAGE        1200 
 #define MAX_CELL_VOLTAGE_ERROR_DIFFERENCE 3000
+#define PWM_STEP                    10
+#define TIMEOUT                     5000
+
 
 void set_pwm(uint16_t set_12V_batt_pwm, uint16_t set_24V_batt_pwm)
 {
@@ -53,43 +58,96 @@ void set_pwm(uint16_t set_12V_batt_pwm, uint16_t set_24V_batt_pwm)
 void balance_cells(void)
 {
     //voltages are in format i.e: 1253 -> 12.53V, 546 -> 5,46V
-  set_pwm(0,0);
-  DELAY_milliseconds(1);
+
+    static uint16_t current_pwm = 0;
+    static uint8_t flag=0;
+
+    set_pwm(0, 0);
+    DELAY_milliseconds(10);
     uint16_t voltage_bus = AutoFox_INA226_GetBusVoltage_V(&ina226);
     uint16_t voltage_batt_12V = Get_ADC_Voltage(ADC_CHANNEL_12V_BATT) / 10; //devide by 10 to get desired resolution, the same as voltage_bus
     uint16_t voltage_batt_24V = voltage_bus - voltage_batt_12V;
-
+    
+    printf("24V:%u mV \r\n", voltage_batt_24V);
+    printf("12V:%u mV \r\n", voltage_batt_12V);
+    printf("BUS INA:%u   \r\n\r\n", voltage_bus);
 
     int16_t cell_voltage_diffrence = (int16_t) voltage_batt_12V - (int16_t) voltage_batt_24V; //calculating voltage difference between batteries
 
-    uint16_t pwm_val = KP * abs(cell_voltage_diffrence);
-    printf("pwm_val: %u \n\r", pwm_val);
-    printf("cell_voltage_diffrence: %u \n\r", abs(cell_voltage_diffrence));
-    if (pwm_val > MAX_CMP_VALUE)
+    uint16_t target_pwm=0;
+
+    if (cell_voltage_diffrence < 0)
     {
-        pwm_val = MAX_CMP_VALUE;
+        target_pwm = KP_24V * abs(cell_voltage_diffrence);
+    }else
+    {
+        target_pwm = KP_12V * abs(cell_voltage_diffrence);
     }
+   
+
+    printf("cell_voltage_diffrence: %u \n\r", abs(cell_voltage_diffrence));
+    printf("current_pwm: %u \n\r", current_pwm);
+    
+    if (target_pwm > MAX_CMP_VALUE)
+    {
+        target_pwm = MAX_CMP_VALUE;
+    }
+
+    if (target_pwm > current_pwm)
+    {
+        current_pwm += PWM_STEP;
+        if (current_pwm > target_pwm)
+        {
+            current_pwm = target_pwm;
+        } 
+    }
+    else
+    {
+        current_pwm -= PWM_STEP;
+        if (current_pwm < target_pwm)
+        {
+            current_pwm = target_pwm;  
+        }      
+    }
+
+
 
     if (abs(cell_voltage_diffrence) < MAX_CELL_VOLTAGE_ERROR_DIFFERENCE)
     {
         if (voltage_batt_12V > MINIMUM_CELL_VOLTAGE && voltage_batt_24V > MINIMUM_CELL_VOLTAGE)
         {
-            if (cell_voltage_diffrence < 0) //if true then voltage for battery 24V has higher voltage, and we need to load this cell
+            if (cell_voltage_diffrence < DEAD_VOLTAGE_DIFFRENCE && balancer_24V_timer==0) //if true then voltage for battery 24V has higher voltage, and we need to load this cell
             {
-                set_pwm(0, pwm_val);
+                balancer_12V_timer=TIMEOUT;
+                set_pwm(0, target_pwm);
                 printf("load 24V \n\r");
-            } else //need to load 12V cell
-            {
-                printf("load 12V \n\r");
-                set_pwm(pwm_val, 0);
+               
             }
-        }else
+            else if (cell_voltage_diffrence > DEAD_VOLTAGE_DIFFRENCE && balancer_12V_timer==0)
+            {
+                balancer_12V_timer=TIMEOUT;
+                set_pwm(target_pwm, 0);
+                printf("load 12V \n\r");
+            }else
+            {
+                set_pwm(0, 0);
+                current_pwm = 0;
+                printf("no balancing - dead zone\n\r");
+            }
+            
+        }
+        else
         {
-            set_pwm(0,0);
-        }     
-    }else
+            set_pwm(0, 0);
+            current_pwm = 0;
+            printf("no balancing - minimum cell voltage not true\n\r");
+        }
+    }
+    else
     {
-         set_pwm(0,0);
+        set_pwm(0, 0);
+        current_pwm = 0;
+        printf("no balancing - too much cell voltage diffrence\n\r");
         // TODO: add error return code    
     }
 }
--- software/Balancer-AVR.X/main.c
+++ software/Balancer-AVR.X/main.c
@@ -9,7 +9,6 @@
 #include "inits.h"
 
 void TCB_ISR_handler(void);
-void TCB_ISRc_handler(void);
 
 AutoFox_INA226 ina226;
 
@@ -24,25 +23,30 @@ int main(void)
 
     while (1)
     {
-        uint16_t voltage_bus_24V, voltage_batt_12V_high, voltage_batt_12V_low;
-
-        voltage_bus_24V = AutoFox_INA226_GetBusVoltage_V(&ina226);
-        voltage_batt_12V_low = Get_ADC_Voltage(ADC_CHANNEL_12V_BATT) / 10;
-        voltage_batt_12V_high = voltage_bus_24V - voltage_batt_12V_low;
-
-        printf("24V:%u mV \r\n", voltage_batt_12V_high);
-        printf("12V:%u mV \r\n", voltage_batt_12V_low);
-        printf("BUS INA:%u   \r\n\r\n", voltage_bus_24V);
-
-      //  DELAY_milliseconds(1);
+     
         balance_cells();
-        printf("TCD0.CMPASET (24V): %d \n\r", TCD0.CMPASET);
-        printf("TCD0.CMPBSET (12V): %d \n\r", TCD0.CMPBSET);
+        
+        
+         //DELAY_milliseconds(1);
     }
 }
 
+static int16_t balancer_timer=0;
+
 void TCB_ISR_handler(void)
 {
+    balancer_12V_timer--;
+    if(balancer_12V_timer < 0)
+    {
+        balancer_12V_timer = 0;
+    }
+
+    balancer_24V_timer--;
+    if(balancer_24V_timer < 0)
+    {
+        balancer_24V_timer = 0;
+    }
+    TCB0.INTFLAGS = TCB_CAPT_bm; // Clear interrupt flag
 }
 
 
